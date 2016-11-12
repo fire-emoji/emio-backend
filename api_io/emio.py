901089f6d0169ad6201caccaa9b098b130999ccf
KadynCBR <kadyn_martinez@hotmail.com>
2016-11-12 03:58:35 -0800

    Added Emio

---
+++ api_io/emio.py
@@ -0,0 +1,26 @@
+#import flask items
+from flask import Flask, request
+from flask_restful import Resource, Api
+#import sqlalchemy for connecting to database
+from sqlalchemy import create_engine
+# json for dumpin'
+import json
+
+# connect flask and api to our app
+emio = Flask(__name__)
+api = Api(emio)
+
+class Emotion_Meta(Resource):
+    def get(self):
+        return "Hello world!"
+
+class Emotion_location(Resource):
+    def get(self, longitude, latitude):
+        j = {'longitude':longitude, 'latitude':latitude, 'Emotion':"Happy!"}
+        return j
+
+api.add_resource(Emotion_location, '/emio/<int:longitude>,<int:latitude>')
+api.add_resource(Emotion_Meta, '/emio/intro')
+
+if __name__ == '__main__':
+    emio.run()
